6a14cb681428b2a9f985e54af6d66bd41c711f59
George-was-here <george.burt@gmail.com>
2022-05-29 00:28:44 +0000

    Adding 5th exercise

---
+++ ex5.rb
@@ -0,0 +1,21 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009 
+height = 74 # inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+kgweight = weight * 0.45359237
+cmheight = height * 2.54
+
+puts "Lets talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are actually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "His weight in KG is #{kgweight}."
+puts "His height in CM is #{cmheight}."
