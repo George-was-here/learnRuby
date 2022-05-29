0adaff5f41cc75d62d56afadba0b85374c89c729
George-was-here <george.burt@gmail.com>
2022-05-29 00:00:20 +0000

    Adding 2nd and 3rd exercises

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+puts "I will now count my chickens:"
+# Take 25 and adds 30 divided by 6 (5)
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Takes 100 and subtracts 25 * 3 % 4
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#Prints title
+puts "Now I will count the eggs:"
+#Runs equation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#Prints comparison string
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0"
+#Runs equation
+puts 3.0 + 2.0 < 5.0 - 7.0
+#Runs 3.0 + 2.0 in insolation
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+#Runs 5- 7 in isolation
+puts "What is 5.0 - 7.0? #{5.0- 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#Compares the value of both integers
+puts "Is it greater? #{5.0 > -2.0}"
+#Compares the value of integers
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+#Compared the value of integers
+puts "Is less or equal? #{5.0 <= -2.0}"
